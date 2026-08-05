7c1b8d72e1065d91ea77ebcfd1c7ae72bae47de4
Fearless-Badger <micah.najacht@gmail.com>
2026-08-05 02:51:17 -0500

    Model

--- src/lucid/models.py
+++ src/lucid/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class PrayerRequest(models.Model):
+    
+    class Status(models.TextChoices):
+        NEW = "nw", "New"
+        PRAYED_FOR = "pr", "Prayed For"
+        ARCHIVED = "arch", "Archived"
+    
+    name = models.CharField(max_length=100, blank=True)
+    request = models.TextField()
+    status = models.CharField(
+        max_length=20,
+        choices=Status.choices,
+        default=Status.NEW
+    )
+    submitted_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
