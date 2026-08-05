60909a49cfa6b77dade8fb80c3eab5209cd544fa
Fearless-Badger <micah.najacht@gmail.com>
2026-08-05 03:09:20 -0500

    Migrate prayerReq

--- src/lucid/models.py
+++ src/lucid/models.py
@@ -1,11 +1,15 @@
+from django.contrib import admin
 from django.db import models
 
 class PrayerRequest(models.Model):
     
     class Status(models.TextChoices):
-        NEW = "nw", "New"
-        PRAYED_FOR = "pr", "Prayed For"
-        ARCHIVED = "arch", "Archived"
+        NEW = "new", "New"
+        PRAYED_FOR = "prayed", "Prayed For"
+        ARCHIVED = "archived", "Archived"
+        
+
+
     
     name = models.CharField(max_length=100, blank=True)
     request = models.TextField()
@@ -16,6 +20,15 @@ class PrayerRequest(models.Model):
     )
     submitted_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self): 
+        return f"{self.name or 'Anonymous'} - {str(self.request)[:40]}" 
 
 
+@admin.register(PrayerRequest)
+class PrayerRequestAdmin(admin.ModelAdmin):
+    list_display = ["name", "status", "submitted_at"]
+    list_filter = ["status"]
+    search_fields = ["name", "request"]
+    readonly_fields = ["request", "name", "submitted_at", "updated_at"]
 
